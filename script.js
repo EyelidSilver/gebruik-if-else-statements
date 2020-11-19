12c4b9b94ad4c4a7ee2bffb46f45f04282e468d6
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2020-11-19 13:32:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const age = 21;
+if (age < 18) {
+    console.log("You are not allowed to drink alcohol, sorry, bye!")
+} else if(age => 18) {
+    console.log("You are allowed to drink alcohol, welcome!")
+}
+const isFemale = true;
+if (isFemale) {
+    console.log ("Welcome to our only ladies night!")
+} else {
+    console.log ("Excuse us but this is only ladies night, see you later!")
+}
+const driverStatus = "not a bob";
+if (driverStatus == "bob") {
+    console.log ("You may drive tonight!")
+} else (
+    console.log ("You may not drive tonight :(")
+)
